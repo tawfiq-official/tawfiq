35309bc72a353da8a120e87b85cbcee46ce2dd2a
Wasim <jaffershaikwasim@gmail.com>
2026-07-08 00:15:15 +0530

    First release

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+  import React from "react";
+  import ReactDOM from "react-dom/client";
+  import App from "./App.jsx";
+  import "./index.css";
 
-import { supabase } from "./lib/supabase";
+  import { supabase } from "./lib/supabase";
 
-console.log("Supabase Client:", supabase);
+  console.log("Supabase Client:", supabase);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
